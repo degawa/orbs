9537fa4d6434cc5734ede19b86eadee138d0687f
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-11-08 15:25:09 +0900

    updated orbs module

--- src/orbs.f90
+++ src/orbs.f90
@@ -1,11 +1,13 @@
 module orbs
     use :: orbs_type_operableBitset
-    use :: orbs_proc_error, orbs_get_error_message => get_error_message
+    use :: orbs_proc_literal
     implicit none
     private
     ! type
     public :: operable_bitset
 
     ! procedures
+    public :: orbs_one
+    public :: orbs_zero
     public :: orbs_get_error_message
 end module orbs
